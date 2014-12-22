fcaa5698be70c47767f48c0ceee0faa989d986c1
dengxinyue0420 <dxy0420@mit.edu>
2014-12-22 14:00:41 +0800

    login screen

---
+++ CoppeWeb/CoppeWeb/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  CoppeWeb
+//
+//  Created by Xinyue Deng on 12/22/14.
+//  Copyright (c) 2014 Yicheng Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
---
+++ CoppeWeb/CoppeWeb/LogInViewController.m
@@ -0,0 +1,37 @@
+//
+//  LogInViewController.m
+//  CoppeWeb
+//
+//  Created by Xinyue Deng on 12/22/14.
+//  Copyright (c) 2014 Yicheng Wang. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
