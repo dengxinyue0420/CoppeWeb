7946ae11118f8493da7095410c9b8f2f8efbb8f3
dengxinyue0420 <dxy0420@mit.edu>
2014-12-27 11:28:27 +0800

    signup logic

---
+++ CoppeWeb/CoppeWeb/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  CoppeWeb
+//
+//  Created by Xinyue Deng on 12/27/14.
+//  Copyright (c) 2014 Yicheng Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
